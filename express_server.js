a857bb9b5bd5646feb030a0bd4a19a74e05e2479
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-12-07 07:09:32 +0000

    updated express_server to include urls get req and created url_index using LHL template

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -11,6 +12,11 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
