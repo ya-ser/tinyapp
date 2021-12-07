c00fd5a46a8ace64f793535986ee2e7ac6c981ca
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-12-07 20:54:00 +0000

    updated server to delete saved urls, added delete button to url index, JSON file now has start script so I can run nodemon on server file for live testing

--- express_server.js
+++ express_server.js
@@ -47,6 +47,13 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+});
+
 // app.get("/hello", (req, res) => {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
 // });
