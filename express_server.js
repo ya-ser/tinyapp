b2d6883b464ad62d032d59bf7be40f34d10add58
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-12-08 03:11:10 +0000

    added login function with cookies

--- express_server.js
+++ express_server.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 app.set("view engine", "ejs");
 
@@ -17,6 +19,12 @@ app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
 });
 
 
