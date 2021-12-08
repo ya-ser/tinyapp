0c65b6d068b1df0546de266b03c076e5106f31c5
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-12-08 04:47:08 +0000

    added logout route to server file, updated renders to include username key, and updated headers with login info

--- express_server.js
+++ express_server.js
@@ -21,12 +21,24 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.get('/urls', (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies.username
+  };
+  res.render('urls_index', templateVars);
+});
+
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
   res.redirect("/urls");
 });
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls/');
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -38,22 +50,29 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies.username };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies.username
+   };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
+  const templateVars = { username: req.cookies.username };
   const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
+  res.redirect(longURL, templateVars);
 });
 
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  const templateVars = { username: req.cookies.username };
+  res.json(urlDatabase, templateVars);
 });
 
 
